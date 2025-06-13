e8d289d5f154d17b791a4601c327f4671e871b2e
sahilparmar6502 <24mca040@nirmauni.ac.in>
2025-06-13 17:24:01 +0530

    CashPaymentStrategy

--- Problems/Parking_Lot/PaymentStrategyPackage/ConcretePaymentStrategy/CashPaymentStrategy.java
+++ Problems/Parking_Lot/PaymentStrategyPackage/ConcretePaymentStrategy/CashPaymentStrategy.java
@@ -2,7 +2,7 @@ package PaymentStrategyPackage.ConcretePaymentStrategy;
 
 import PaymentStrategyPackage.PaymentStrategy;
 
-public class CashPaymentStrategy {
+public class CashPaymentStrategy implements PaymentStrategy{
     public void processPayment(double amount){
         System.out.println("Processing payment by cash of $"+amount);
     }
